03a60411ce5e7e99a2d7879cad62b86f569f8219
Aashishbirhade <134684645+Aashishbirhade@users.noreply.github.com>
2023-05-27 11:19:15 +0530

    Add files via upload

---
+++ Day1_Arithmatic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter any two number";
+    cin>>a>>b;
+    cout<<"Addtion: "<<a<<" "<<"+ "<<b<<" = "<<a+b<<endl;
+    cout<<"Substraction: "<<a<<" "<<"- "<<b<<" = "<<a-b<<endl;
+    cout<<"Multiplication: "<<a<<" "<<"* "<<b<<" = "<<a*b<<endl;
+    cout<<"Division: "<<a<<" "<<"/ "<<b<<" = "<<a/b<<endl;
+     cout<<"Moduls: "<<a<<" "<<"% "<<b<<" = "<<a%b<<endl;
+    
+}
