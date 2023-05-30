d6a8c60e79f7d008ffab1da0d9a5770f26a30086
Aashishbirhade <134684645+Aashishbirhade@users.noreply.github.com>
2023-05-30 13:29:06 +0530

    Add files via upload

---
+++ Day4_ASCII.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+   char e;
+    cout<<"enter any key : ";
+    cin>>e;
+    cout<<"ASCII value of given Number,Charecter, Symbol is "<<int(e)<<endl;
+    int a;
+    float b;
+    char c;
+    double d;
+
+    cout<<"size of integer "<<sizeof(a)<<" byte"<<endl;
+    cout<<"size of float "<<sizeof(b)<<" byte"<<endl;
+    cout<<"size of char "<<sizeof(c)<<" byte"<<endl;
+    cout<<"size of double "<<sizeof(d)<<" byte"<<endl;
+   
+
+
+}
