96f965918ad15ac03bb0590df7709821496c28a3
Aashishbirhade <134684645+Aashishbirhade@users.noreply.github.com>
2023-06-01 13:30:10 +0530

    Add files via upload

---
+++ Day6_operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int a,b;
+ cout<<"enter any two number :";
+ cin>>a>>b;
+ cout<<"number First is greater then number second : "<<(a>b)<<endl;
+ cout<<"number First is less then number second : "<<(a<b)<<endl;
+ cout<<"number First and number second both are equal : "<<(a==b)<<endl;
+ 
+}
